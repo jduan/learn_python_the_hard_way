c49c9688db17750af325b8f98b172a1d430295e9
Jingjing Duan <jingjing.duan@hulu.com>
2013-12-22 11:00:42 -0800

    ex11.py

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print "How old are you?",
+age = raw_input(" (years) ")
+print "How tall are you?",
+height = raw_input(" (inches) ")
+print "How much do you weight?",
+weight = raw_input(" (lbs) ")
+print "Where do you live?",
+address = raw_input(" (city) ")
+
+print "So, you are %s old, %s tall, %s heavy, and you live in %s." % (
+        age, height, weight, address)
